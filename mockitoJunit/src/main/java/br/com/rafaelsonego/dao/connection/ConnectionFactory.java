b1e45e7efab188bddc308bae4b03cc003ef70cee
Rafael Sônego da Silva <rafael2785@gmail.com>
2017-06-16 22:34:27 -0400

    Update database

--- mockitoJunit/src/main/java/br/com/rafaelsonego/dao/connection/ConnectionFactory.java
+++ mockitoJunit/src/main/java/br/com/rafaelsonego/dao/connection/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 	
 	private static Connection connection;
-	private static String pathDataDase = "C:\\Users\\rafae\\WORKSPACE\\MockitoJunit\\mockitoJunit\\src\\main\\resources\\db\\database\\databaseTest";
+	private static String pathDataDase = "C:\\Users\\rafae\\OneDrive\\Documentos\\GitHub\\MockitoJUnit\\mockitoJunit\\src\\main\\resources\\db\\database\\databaseTest";
 	private static String URL = "jdbc:hsqldb:file:" + pathDataDase;
 	private static String user = "SA";
 	private static String password = "";
